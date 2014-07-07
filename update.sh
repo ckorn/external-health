6e1122114453492e12b6b05068eff05cf9984a90
Christoph Korn <c_korn@gmx.de>
2014-07-07 03:00:40 +0200

    Workaround SSL certificate verification bug in wget
    qa.debian.org now always uses the https version.
    This has triggered an old bug in wget which still exists
    in Ubuntu 12.04 [1][2]. This workaround disables SSL
    certificate verification until the server is updated
    to Ubuntu 14.04.
    
    http://savannah.gnu.org/bugs/?23934
    https://bugs.launchpad.net/ubuntu/+source/wget/+bug/733888

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+export PERL_LWP_SSL_VERIFY_HOSTNAME=0
 python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/trusty-getdeb/apps/source/Sources.gz" getdeb.html
 python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/trusty-getdeb/games/source/Sources.gz" playdeb.html

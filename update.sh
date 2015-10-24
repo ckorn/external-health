fb84ae076e3eb3b461081ed844a9bdc335f0f51e
Christoph Korn <christoph.korn@posteo.de>
2015-10-24 18:09:33 +0200

    Change repository to wily
    Ubuntu 15.10 is released.
    trusty and wily will be supported.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PERL_LWP_SSL_VERIFY_HOSTNAME=0
-python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/vivid-getdeb/apps/source/Sources.gz" getdeb.html
-python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/vivid-getdeb/games/source/Sources.gz" playdeb.html
+python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/wily-getdeb/apps/source/Sources.gz" getdeb.html
+python getdeb_external_health.py "http://188.138.90.189/ubuntu/" "http://188.138.90.189/ubuntu/dists/wily-getdeb/games/source/Sources.gz" playdeb.html
